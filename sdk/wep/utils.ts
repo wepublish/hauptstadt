0da5eae809c26b5634a506ef95af7a4361bcdc3f
Lukas_Skywalker <git@lukasdiener.ch>
2024-05-13 16:23:24 +0200

    HAS-16: Do not cut articles when visiting from a cafe
    This change allows cafes to provide their guests with a secret key that allows
    them to read the entire article instead of cutting them off. The key is passed
    as a query param at the first visit and stored in a cookie, valid for 4 hours.
    When the cookie is set, the entire article is shown. The cookie is checked
    whenever a link to an article link is rendered.
    
    The key can be provided by appending ?key=example to the URL of the home page.

--- sdk/wep/utils.ts
+++ sdk/wep/utils.ts
@@ -1,3 +1,32 @@
 export function stripHtml(input: string) {
   return input.replace(/(<([^>]+)>)/gi, '')
 }
+
+export class LoginBypass {
+  private COOKIE_NAME = 'cafe-key'
+  private VALID_KEYS = {
+    'DUMMY_CAFE': 'aed1dfa6dd5020ab7af56aebd2e5db68',
+  }
+
+  constructor(private $cookies: any) {}
+
+  enable(key: string): boolean {
+    if (!key || !this.valid(key)) {
+      return false
+    }
+    this.$cookies.set(this.COOKIE_NAME, key, {
+      path: '/',
+      maxAge: 60 * 60 * 4
+    })
+    return true
+  }
+
+  check(): boolean {
+    const cookie = this.$cookies.get(this.COOKIE_NAME)
+    return this.valid(cookie)
+  }
+
+  private valid(value: string): boolean {
+    return Object.values(this.VALID_KEYS).indexOf(value) > -1
+  }
+}
