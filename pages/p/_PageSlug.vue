302f8ced4cb51be07ef292655c6f883773d180ea
michael-scheurer <70939054+michael-scheurer@users.noreply.github.com>
2024-10-23 13:45:25 +0200

    add old page redirect (#22)

---
+++ pages/p/_PageSlug.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  asyncData ({redirect, route}) {
+    const {PageSlug} = route.params
+    if (PageSlug) {
+      redirect(`/${PageSlug}`)
+    }
+  }
+})
+</script>
